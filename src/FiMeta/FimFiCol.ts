f3e9d0c170ba10bd806e7970f4c66abc1e40db08
TuncayHp2 <tunc270@gmail.com>
2026-02-11 20:43:12 +0300

    up

--- src/FiMeta/FimFiCol.ts
+++ src/FiMeta/FimFiCol.ts
@@ -3,104 +3,104 @@ import { FiMeta } from "../FiContainers/FiMeta";
 
 export class FimFiCol {
 
-  public static ofcTxDesc(): FiMeta {
-    let fiMeta = FiMeta.create("ofcTxDesc");
+  public static fcTxDesc(): FiMeta {
+    let fiMeta = FiMeta.create("fcTxDesc");
 
     return fiMeta;
   }
 
-  public static ofcTxPrefix(): FiMeta {
-    let fiMeta = FiMeta.create("ofcTxPrefix");
+  public static fcTxPrefix(): FiMeta {
+    let fiMeta = FiMeta.create("fcTxPrefix");
 
     return fiMeta;
   }
 
-  public static ofcTxEntityName(): FiMeta {
-    let fiMeta = FiMeta.create("ofcTxEntityName");
+  public static fcTxEntityName(): FiMeta {
+    let fiMeta = FiMeta.create("fcTxEntityName");
 
     return fiMeta;
   }
 
-  public static ofcTxFieldName(): FiMeta {
-    let fiMeta = FiMeta.create("ofcTxFieldName");
+  public static fcTxFieldName(): FiMeta {
+    let fiMeta = FiMeta.create("fcTxFieldName");
 
     return fiMeta;
   }
 
-  public static ofcTxFieldType(): FiMeta {
-    let fiMeta = FiMeta.create("ofcTxFieldType");
+  public static fcTxFieldType(): FiMeta {
+    let fiMeta = FiMeta.create("fcTxFieldType");
 
     return fiMeta;
   }
 
-  public static ofcTxHeader(): FiMeta {
-    let fiMeta = FiMeta.create("ofcTxHeader");
+  public static fcTxHeader(): FiMeta {
+    let fiMeta = FiMeta.create("fcTxHeader");
 
     return fiMeta;
   }
 
-  public static ofcTxDbField(): FiMeta {
-    let fiMeta = FiMeta.create("ofcTxDbField");
+  public static fcTxDbField(): FiMeta {
+    let fiMeta = FiMeta.create("fcTxDbField");
 
     return fiMeta;
   }
 
-  public static ofcTxRefField(): FiMeta {
-    let fiMeta = FiMeta.create("ofcTxRefField");
+  public static fcTxRefField(): FiMeta {
+    let fiMeta = FiMeta.create("fcTxRefField");
 
     return fiMeta;
   }
 
-  public static ofcLnLength(): FiMeta {
-    let fiMeta = FiMeta.create("ofcLnLength");
+  public static fcLnLength(): FiMeta {
+    let fiMeta = FiMeta.create("fcLnLength");
 
     return fiMeta;
   }
 
-  public static ofcLnPrecision(): FiMeta {
-    let fiMeta = FiMeta.create("ofcLnPrecision");
+  public static fcLnPrecision(): FiMeta {
+    let fiMeta = FiMeta.create("fcLnPrecision");
 
     return fiMeta;
   }
 
-  public static ofcLnScale(): FiMeta {
-    let fiMeta = FiMeta.create("ofcLnScale");
+  public static fcLnScale(): FiMeta {
+    let fiMeta = FiMeta.create("fcLnScale");
 
     return fiMeta;
   }
 
-  public static ofcBoNullable(): FiMeta {
-    let fiMeta = FiMeta.create("ofcBoNullable");
+  public static fcBoNullable(): FiMeta {
+    let fiMeta = FiMeta.create("fcBoNullable");
 
     return fiMeta;
   }
 
-  public static ofcTxIdType(): FiMeta {
-    let fiMeta = FiMeta.create("ofcTxIdType");
+  public static fcTxIdType(): FiMeta {
+    let fiMeta = FiMeta.create("fcTxIdType");
 
     return fiMeta;
   }
 
-  public static ofcBoTransient(): FiMeta {
-    let fiMeta = FiMeta.create("ofcBoTransient");
+  public static fcBoTransient(): FiMeta {
+    let fiMeta = FiMeta.create("fcBoTransient");
 
     return fiMeta;
   }
 
-  public static ofcBoUnique(): FiMeta {
-    let fiMeta = FiMeta.create("ofcBoUnique");
+  public static fcBoUnique(): FiMeta {
+    let fiMeta = FiMeta.create("fcBoUnique");
 
     return fiMeta;
   }
 
-  public static ofcBoUniqGro1(): FiMeta {
-    let fiMeta = FiMeta.create("ofcBoUniqGro1");
+  public static fcBoUniqGro1(): FiMeta {
+    let fiMeta = FiMeta.create("fcBoUniqGro1");
 
     return fiMeta;
   }
 
-  public static ofcTxDefValue(): FiMeta {
-    let fiMeta = FiMeta.create("ofcTxDefValue");
+  public static fcTxDefValue(): FiMeta {
+    let fiMeta = FiMeta.create("fcTxDefValue");
 
     return fiMeta;
   }
